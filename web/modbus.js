f8c30e71e6de6a245f35ec542a4c8562c12b8bf3
ksb <ksb@users.sourceforge.net>
2023-01-29 19:39:44 +0100

    Improved handling of input on web page

--- web/modbus.js
+++ web/modbus.js
@@ -12,19 +12,21 @@ function socket_uri() {
 }
 const MB_NS = "http://www.elektro-kapsel.se/xml/mb-tool";
 var value_map = {};
-
+var mb_values = {};
 function update_value(a, v) {
     let inps = value_map[a];
     if (inps) {
         for (inp of inps) {
-            let low = inp.getAttributeNS(MB_NS, "bit_low");
-            let high = inp.getAttributeNS(MB_NS, "bit_high");
-            if (low != null && high != null) {
-                inp.value = (v >> low) & ((1 << (high - low + 1)) - 1);
-            } else {
-                inp.value = v;
+	    if (!(inp === document.activeElement)) {
+		let low = inp.getAttributeNS(MB_NS, "bit_low");
+		let high = inp.getAttributeNS(MB_NS, "bit_high");
+		if (low != null && high != null) {
+                    inp.value = (v >> low) & ((1 << (high - low + 1)) - 1);
+		} else {
+                    inp.value = v;
+		}
             }
-        }
+	}
     }
 }
 
@@ -32,19 +34,30 @@ function setup() {
     var regs = document.getElementById("holding_registers");
     var values = regs.getElementsByClassName("mb_value");
     for (v of values) {
-
         let addr = parseInt(v.getAttributeNS(MB_NS, "addr"));
         console.log(addr);
         if (value_map[addr]) {
-            value_map[addr].push(v);
+	    value_map[addr].push(v);
         } else {
-            value_map[addr] = [v];
+	    value_map[addr] = [v];
         }
+	let inp = v;
         v.addEventListener("change", function(e) {
-
-            ws.send(JSON.stringify({ UpdateHoldingRegs: { start: addr, regs: [parseInt(e.target.value)] } }))
-            update_value(addr, e.target.value);
+	    let low = inp.getAttributeNS(MB_NS, "bit_low");
+	    let high = inp.getAttributeNS(MB_NS, "bit_high");
+	    let value = parseInt(e.target.value);
+	    if (low != null && high != null) {
+		let old_value = mb_values[addr] || 0;
+		let mask = ((1 << (high - low + 1)) - 1) << low;
+                value = (old_value & ~mask) | (value << low) & mask;
+	    }
+	    ws.send(JSON.stringify({
+		UpdateHoldingRegs: {
+		    start: addr, regs: [value]
+		} }))
+	    update_value(addr, value);
         });
+	
     }
     ws = new WebSocket(socket_uri());
     ws.onmessage = (msg) => {
@@ -53,12 +66,13 @@ function setup() {
         if (holding_registers) {
             let a = holding_registers.start;
             for (v of holding_registers.regs) {
+		mb_values[a] = v;
                 update_value(a, v);
                 a++;
 
             }
         }
-        console.log(msg)
+//        console.log(msg)
 
     };
     ws.onopen = () => {
